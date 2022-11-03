a2814115a249a27d24ae7d2266356b3aae0871b5
unknown <sendevshruti@gmail.com>
2022-11-03 12:38:55 +0530

    code_corrected

--- Prime_Problem.js
+++ Prime_Problem.js
@@ -1,13 +1,19 @@
 function identifyPrime(num) {
     let count = 0;
  for(let i=0;i<num;i++){
-   if(num%i = 1){
+   if(num%i == 0){
           count++;
          
         }
    }if(count==2){
-   console.log('Yes');
-   }else
-     console.log('No');
-    
+  return true;
+   }
+return false;
+   }
+let ans=identifyPrime(53);
+
+if (ans==true){
+console.log("Prime Number");
+}else{
+console.log("Not a Prime Number");
 }
